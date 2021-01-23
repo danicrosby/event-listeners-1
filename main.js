e8f03a9743862a1a75eb7336e68f13720ed7be8e
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2021-01-23 11:08:35 -0600

    working with callbacks

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-console.log('CONNECTED!');
+// console.log('CONNECTED!');
+
+function saysHelloToUser(user) {
+  return `Hello ${user}!`;
+}
+
+function saysGoodbyeToUser(user) {
+  return `Goodbye ${user}!`;
+}
+
+// A FUNCTION THAT TAKES IN A FUNCTION AS AN ARGUMENT
+function createGreeting(user, cb) {
+  return cb(user);
+}
+
+
+// CONSOLE LOGGING THE RETURN STATEMENTS OF EACH CALLBACK
+console.log(createGreeting('Dan', saysHelloToUser)); // 'Hello Dan!'
+console.log(createGreeting('Dan', saysGoodbyeToUser)); // 'Goodbye Dan!'
