71951ecce17dce8a10206974d633e0987f1ccff8
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2021-01-23 10:56:58 -0600

    tested selectors

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
-console.log('CONNECTED!');
+const mainBtn = document.getElementById('main-btn');
+console.log(mainBtn);
+
+const mainBtn2 = document.querySelector('#main-btn');
+console.log(mainBtn2);
